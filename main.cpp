221a1db94fff28ce87e1b3c979f19d647b23de97
jakubmelnik <jakubmelnik@gmail.com>
2026-05-18 23:31:14 +0200

    finished euler algorithms and working result.

--- main.cpp
+++ main.cpp
@@ -2,22 +2,18 @@
 #include <vector>
 #include <fstream>
 
-bool hamiltonRecursiveAdjacent(int pos, int V, const std::vector<std::vector<bool>>& adjMatrix, std::vector<int>& path, std::vector<bool>& visited)
+bool hamiltonRecursiveAdjacent(int pos, const std::vector<std::vector<bool>>& adjMatrix, std::vector<int>& path, std::vector<bool>& visited)
 {
-	if (pos == V)
-	{
-		if (adjMatrix[path[pos - 1]][0]) return true;
-		return false;
-	}
+	if (pos == adjMatrix.size()) return adjMatrix[path[pos - 1]][0];
 
-	for (int v = 0; v < V; v++)
+	for (int v = 0; v < adjMatrix.size(); v++)
 	{
 		if (adjMatrix[path[pos - 1]][v] && !visited[v])
 		{
 			path[pos] = v;
 			visited[v] = true;
 			
-			if (hamiltonRecursiveAdjacent(pos + 1, V, adjMatrix, path, visited)) return true;
+			if (hamiltonRecursiveAdjacent(pos + 1, adjMatrix, path, visited)) return true;
 
 			visited[v] = false;
 		}
@@ -25,9 +21,9 @@ bool hamiltonRecursiveAdjacent(int pos, int V, const std::vector<std::vector<boo
 	return false;
 }
 
-bool hamiltonRecursiveSuccession(int pos, int V, const std::vector<std::vector<int>>& succList, std::vector<int>& path, std::vector<bool>& visited)
+bool hamiltonRecursiveSuccession(int pos, const std::vector<std::vector<int>>& succList, std::vector<int>& path, std::vector<bool>& visited)
 {
-	if (pos == V)
+	if (pos == succList.size())
 	{
 		for (int n : succList[path[pos - 1]])
 		{
@@ -43,7 +39,7 @@ bool hamiltonRecursiveSuccession(int pos, int V, const std::vector<std::vector<i
 			path[pos] = v;
 			visited[v] = true;
 
-			if (hamiltonRecursiveSuccession(pos + 1, V, succList, path, visited)) return true;
+			if (hamiltonRecursiveSuccession(pos + 1, succList, path, visited)) return true;
 
 			visited[v] = false;
 		}
@@ -51,30 +47,91 @@ bool hamiltonRecursiveSuccession(int pos, int V, const std::vector<std::vector<i
 	return false;
 }
 
-void initHamiltonAdjacent(int V, const std::vector<std::vector<bool>>& adjMatrix)
+bool eulerRecursiveAdjacent(int pos, int E, std::vector<std::vector<bool>>& adjMatrix, std::vector<int>& path)
 {
-	std::vector<int> path(V, -1);
-	std::vector<bool> visited(V, false);
+	if (pos == E + 1) return path[pos - 1] == 0;
 
-	path[0] = 0;
-	visited[0] = true;
+	for (int v = 0; v < adjMatrix.size(); v++)
+	{
+		if (adjMatrix[path[pos - 1]][v])
+		{
+			path[pos] = v;
 
-	std::cout << "macierz sasiedztwa: " << (hamiltonRecursiveAdjacent(1, V, adjMatrix, path, visited) ? "Jest cykl" : "Nie ma cyklu");
+			adjMatrix[path[pos - 1]][v] = false;
+			adjMatrix[v][path[pos - 1]] = false;
+
+			if (eulerRecursiveAdjacent(pos + 1, E, adjMatrix, path)) return true;
+
+			adjMatrix[path[pos - 1]][v] = true;
+			adjMatrix[v][path[pos - 1]] = true;
+		}
+	}
+	return false;
 }
 
-void initHamiltonSuccession(int V, const std::vector<std::vector<int>>& succList)
+bool eulerRecursiveSuccession(int pos, int E, std::vector<std::vector<int>>& succList, std::vector<int>& path)
 {
-	std::vector<int> path(V, -1);
-	std::vector<bool> visited(V, false);
+	if (pos == E + 1) return path[pos - 1] == 0;
+
+	for (int& v : succList[path[pos - 1]])
+	{
+		if (v != -1)
+		{
+			int t = v;
+			path[pos] = t;
+
+			v = -1;
+
+			if (eulerRecursiveSuccession(pos + 1, E, succList, path)) return true;
+			v = t;
+		}
+	}
+	return false;
+}
+
+void initHamiltonAdjacent(const std::vector<std::vector<bool>>& adjMatrix)
+{
+	std::vector<int> path(adjMatrix.size(), -1);
+	std::vector<bool> visited(adjMatrix.size(), false);
 
 	path[0] = 0;
 	visited[0] = true;
 
-	std::cout << "skierowana lista: " << (hamiltonRecursiveSuccession(1, V, succList, path, visited) ? "Jest cykl" : "Nie ma cyklu") << std::endl;
+	std::cout << "Hamilton macierz sasiedztwa: " << (hamiltonRecursiveAdjacent(1, adjMatrix, path, visited) ? "Jest cykl" : "Nie ma cyklu") << std::endl;
+}
+
+void initHamiltonSuccession(const std::vector<std::vector<int>>& succList)
+{
+	std::vector<int> path(succList.size(), -1);
+	std::vector<bool> visited(succList.size(), false);
+
+	path[0] = 0;
+	visited[0] = true;
+
+	std::cout << "Hamilton lista skierowana: " << (hamiltonRecursiveSuccession(1, succList, path, visited) ? "Jest cykl" : "Nie ma cyklu") << std::endl;
+}
+
+void initEulerAdjacent(int E, std::vector<std::vector<bool>> adjMatrix)
+{
+	std::vector<int> path(E + 1, -1);
+	path[0] = 0;
+
+	std::cout << "Euler macierz sasiedztwa: " << (eulerRecursiveAdjacent(1, E, adjMatrix, path) ? "Jest cykl" : "Nie ma cyklu") << std::endl;
+}
+
+void initEulerSuccession(int E, std::vector<std::vector<int>> succList)
+{
+	std::vector<int> path(E + 1, -1);
+	path[0] = 0;
+
+	std::cout << "Euler lista skierowana: " << (eulerRecursiveSuccession(1, E, succList, path) ? "Jest cykl" : "Nie ma cyklu") << std::endl;
 }
 
 int main()
 {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+
 	std::ifstream graphFile("graph.txt");
 
 	int V, E;
@@ -94,6 +151,8 @@ int main()
 		succList[u].push_back(v);
 	}
 
-	initHamiltonAdjacent(V, adjMatrix);
-	initHamiltonSuccession(V, succList);
+	initHamiltonAdjacent(adjMatrix);
+	initHamiltonSuccession(succList);
+	initEulerAdjacent(E,adjMatrix);
+	initEulerSuccession(E, succList);
 }
